9845ca6d60c57ec4acb59863407d34f6a99bbcfe
erikqb3 <erikqb3@gmail.com>
2021-10-26 17:01:15 -0600

    Pokemon Cafe css fix

--- Pokemon_Ro-Sham-Bo!/Pokemon(draft2).js
+++ Pokemon_Ro-Sham-Bo!/Pokemon(draft2).js
@@ -52,7 +52,8 @@ function getRandomPokemon (path) {
 //function pathResults(path, number, url)
    
   if (path === 1) { 
-    fetch(url).then(convertToJson).then(yourPokemon); 
+    clearPokemon();
+    fetch(url).then(response => response.json()).then(yourPokemon); 
   }
   else if (path === 2) {
     yourPokemon = document.getElementById("yourPokemon_img").children;
@@ -61,192 +62,223 @@ function getRandomPokemon (path) {
       alert("You need to get Pokemon!")
     }
     else {
-      fetch(url).then(convertToJson).then(otherPokemon);
+      fetch(url).then(response => response.json()).then(otherPokemon).then(prepForCalculations);
     }
   }
   else if (path === 3) {
-    let done = false;
-    while (!done) {
-      fetch(url).then(convertToJson).then(yourPokemon);
-      done = true;
-    }
+    clearPokemon();
     let otherNumber = Math.floor(Math.random() * 899);
     const otherUrl = `https://pokeapi.co/api/v2/pokemon/${otherNumber}`;
-    fetch(otherUrl).then(convertToJson).then(otherPokemon); 
+
+
+    // fetch(url).then(response => response.json()).then(yourPokemon)
+    // fetch(otherUrl).then(response => response.json()).then(otherPokemon); 
+
+    Promise.all([fetch(url).then(response => response.json()).then(yourPokemon),
+      fetch(otherUrl).then(response => response.json()).then(otherPokemon)]).then(prepForCalculations)
+
+
   }
+}
+
+// function convertToJson(jsonData) {
+//   if (jsonData.ok) {
+//     return jsonData.json();
+//   } else {
+//     console.log("error:", jsonData);
+//   }
+// }
+
+function choosePokemon(results) {
+  let type1;
+  let type2;
   
-  
-  function convertToJson(jsonData) {
-    if (jsonData.ok) {
-      return jsonData.json();
-    } else {
-      console.log("error:", jsonData);
-    }
+  let pokemonName = `<h2>${results.name}</h2>`;
+  let pokemonSize = `<p>${results.height}</p>`
+  console.log(pokemonName, pokemonSize)
+  // console.log(pokemonSize)
+  type1 = `<p>${results.types[0].type.name}</p>`;
+  try {
+    type2 = `<p>${results.types[1].type.name}</p>`;
   }
-  
-  function choosePokemon(convertedData) {
-    const outputElement = document.querySelector('#output');
-    // console.log(data)
-    results = convertedData;
-    let type1;
-    let type2;
+  catch (err) {
+    type2 = null;
+  }
+    let pokemonImg = `<img src="${results.sprites.front_default}" alt="Image of ${results.name}" width="200">`;
+    let damageDelt = `<p class="damageCount">Damage Delt = 0</p>`
+
+
+    let pokemonInfo = [pokemonName,type1,type2,pokemonImg,pokemonSize,damageDelt];
     
-    let pokemonName = `<h2>${results.name}</h2>`;
-    let pokemonSize = `<p>${results.height}</p>`
-    // console.log(pokemonSize)
-    type1 = `<p>${results.types[0].type.name}</p>`;
-    try {
-      type2 = `<p>${results.types[1].type.name}</p>`;
-    }
-    catch (err) {
-      type2 = null;
-    }
-      let pokemonImg = `<img src="${results.sprites.front_default}" alt="Image of ${results.name}" width="200">`;
-      let damageDelt = `<p class="damageCount">Damage Delt = 0</p>`
-
-
-      let pokemonInfo = [pokemonName,type1,type2,pokemonImg,pokemonSize,damageDelt];
-      
-      
-      return pokemonInfo;
-  }
     
-  function yourPokemon(response) {
-    let jsonData = response;
-    let yourPokemon = choosePokemon(jsonData);
-    let htmlStorage = document.getElementById("yourPokemon_info");
-    // console.log(yourPokemon);
-    // console.log(htmlStorage);
-    if (yourPokemon[2] != null) {
-      htmlStorage.innerHTML = yourPokemon[0].toUpperCase() + yourPokemon[4] + yourPokemon[1] + yourPokemon[2] + yourPokemon[5];
-    }
-    else {
-      htmlStorage.innerHTML = yourPokemon[0].toUpperCase() + yourPokemon[4] + yourPokemon[1] + yourPokemon[5];
-    }
-
-    document.getElementById("yourPokemon_img").innerHTML = yourPokemon[3];
-
-    let otherPokemon = document.getElementById("otherPokemon_img").children;
-    // console.log(otherPokemon)
-    if (otherPokemon.length != 0) {
-      document.getElementById("otherPokemon_img").innerHTML = "";
-      document.getElementById("otherPokemon_info").innerHTML = "";
-    }
-
-  }
-  
-  function otherPokemon(response) {
-    let jsonData = response;
-    // console.log(response)
-    let otherPokemon = choosePokemon(jsonData);
-    let htmlStorage = document.getElementById("otherPokemon_info");
-    // console.log(otherPokemon);
-    // console.log(htmlStorage);
-    if (otherPokemon[2] != null) {
-      htmlStorage.innerHTML = otherPokemon[0].toUpperCase() + otherPokemon[4] + otherPokemon[1] + otherPokemon[2] + otherPokemon[5];
-    }
-    else {
-      htmlStorage.innerHTML = otherPokemon[0].toUpperCase() + otherPokemon[4] + otherPokemon[1] + otherPokemon[5];
-    }
-    document.getElementById("otherPokemon_img").innerHTML = otherPokemon[3];
-
-    yourPokemon_type1 = document.getElementById("yourPokemon_info").children[2];
-    yourPokemon_type2 = document.getElementById("yourPokemon_info").children[3];
-    yourPokemon_size = parseInt(document.getElementById("yourPokemon_info").children[1].innerHTML);
-    otherPokemon_type1 = document.getElementById("otherPokemon_info").children[2];
-    otherPokemon_type2 = document.getElementById("otherPokemon_info").children[3];
-    otherPokemon_size = parseInt(document.getElementById("otherPokemon_info").children[1].innerHTML);
-
-    let competingTypes = [yourPokemon_type1, yourPokemon_type2, otherPokemon_type1, otherPokemon_type2];
-    let competingSizes = [yourPokemon_size,otherPokemon_size];
-    // console.log(competingTypes)
-    // console.log(competingTypes[0].innerHTML)
-    for (i=0; i < competingTypes.length; i++) {
-      if (competingTypes[i] != undefined) {
-        competingTypes[i] = competingTypes[i].innerHTML;
-      }
-      else {
-        competingTypes[i] = null;
-      }
-    }
-
-    timeToFight(competingTypes,competingSizes);
-    // console.log(yourPokemon[3])
-
-  }
-
-  function timeToFight (competingTypes,competingSizes) {
-    let yourDamageDelt = (checkTypeAdvantage(competingTypes[0],competingTypes[2])) * (checkTypeAdvantage(competingTypes[0],competingTypes[3])) + (checkTypeAdvantage(competingTypes[1],competingTypes[2])) * (checkTypeAdvantage(competingTypes[1],competingTypes[3]));
-    let otherDamageDelt = (checkTypeAdvantage(competingTypes[2],competingTypes[0])) * (checkTypeAdvantage(competingTypes[2],competingTypes[1])) + (checkTypeAdvantage(competingTypes[3],competingTypes[0])) * (checkTypeAdvantage(competingTypes[3],competingTypes[1]));
-
-    console.log("yourDamageDelt = " +yourDamageDelt)
-    console.log("otherDamageDelt = " +otherDamageDelt)
-
-    if (competingSizes[0] > competingSizes[1]) {
-      yourDamageDelt = yourDamageDelt + 0.5;
-    }
-    else if (competingSizes[0] < competingSizes[1]) {
-      otherDamageDelt = otherDamageDelt + 0.5;
-    }
-    else (
-      console.log("tie")
-    )
-
-      infoCount_you = document.getElementById("yourPokemon_info");
-      infoCount_other = document.getElementById("otherPokemon_info");
-      // console.log(infoCount_you)
-
-    let damageYou = `Damage Delt = ${yourDamageDelt}`;
-    let damageOther = `Damage Delt = ${otherDamageDelt}`;
-
-    // console.log(yourDamageDelt);
-    // console.log(otherDamageDelt);
-
-    // console.log("children length you = " + infoCount_you.children.length);
-    // console.log("children length other = " + infoCount_other.children.length)
-
-    if (infoCount_you.children.length == 5) {
-      document.getElementById("yourPokemon_info").children[4].textContent = damageYou;
-    }
-    else {
-      document.getElementById("yourPokemon_info").children[3].textContent = damageYou;
-    }
-    if (infoCount_other.children.length == 5) {
-      document.getElementById("otherPokemon_info").children[4].textContent = damageOther;
-    }
-    else {
-      document.getElementById("otherPokemon_info").children[3].textContent = damageOther;
-    }
-
-
-    // document.getElementById("yourPokemon_info").children[3].textContent = damageYou;
-    // document.getElementById("otherPokemon_info").children[3].textContent = damageOther;
-
-    // console.log(document.getElementById("otherPokemon_info").children[3]);
-    // console.log(damageYou)
-
-    scoreChange(yourDamageDelt,otherDamageDelt);
-  }
-
-  function scoreChange (yourDamageDelt,otherDamageDelt) {
-    let yourScore = parseInt(document.getElementById("yourPoints").textContent);
-    let otherScore = parseInt(document.getElementById("otherPoints").innerHTML);
-    if (yourDamageDelt > otherDamageDelt) {
-      yourScore += 1;
-      document.getElementById("yourPoints").textContent = yourScore;
-    }
-    else if (yourDamageDelt < otherDamageDelt) {
-      otherScore += 1;
-      document.getElementById("otherPoints").innerHTML = otherScore;
-    }
-    else {
-      console.log("tie");
-    }
-
-    // console.log(yourScore,otherScore)
-  };
-
-
+    return pokemonInfo;
 }
   
+function yourPokemon(jsonData) {
+  let yourPokemon = choosePokemon(jsonData);
+  let htmlStorage = document.getElementById("yourPokemon_info");
+  // console.log(yourPokemon);
+  // console.log(htmlStorage);
+  if (yourPokemon[2] != null) {
+    htmlStorage.innerHTML = yourPokemon[0].toUpperCase() + yourPokemon[4] + yourPokemon[1] + yourPokemon[2] + yourPokemon[5];
+  }
+  else {
+    htmlStorage.innerHTML = yourPokemon[0].toUpperCase() + yourPokemon[4] + yourPokemon[1] + yourPokemon[5];
+  }
+
+  document.getElementById("yourPokemon_img").innerHTML = yourPokemon[3];
+
+  // let otherPokemon = document.getElementById("otherPokemon_img").children;
+  // // console.log(otherPokemon)
+  // if (otherPokemon.length != 0) {
+  //   document.getElementById("otherPokemon_img").innerHTML = "";
+  //   document.getElementById("otherPokemon_info").innerHTML = "";
+  // }
+
+}
+
+function otherPokemon(jsonData) {
+  let otherPokemon = choosePokemon(jsonData);
+  let htmlStorage = document.getElementById("otherPokemon_info");
+  // console.log(otherPokemon);
+  // console.log(htmlStorage);
+  if (otherPokemon[2] != null) {
+    htmlStorage.innerHTML = otherPokemon[0].toUpperCase() + otherPokemon[4] + otherPokemon[1] + otherPokemon[2] + otherPokemon[5];
+  }
+  else {
+    htmlStorage.innerHTML = otherPokemon[0].toUpperCase() + otherPokemon[4] + otherPokemon[1] + otherPokemon[5];
+  }
+  document.getElementById("otherPokemon_img").innerHTML = otherPokemon[3];
+
+  // yourPokemon_type1 = document.getElementById("yourPokemon_info").children[2];
+  // yourPokemon_type2 = document.getElementById("yourPokemon_info").children[3];
+  // yourPokemon_size = parseInt(document.getElementById("yourPokemon_info").children[1].innerHTML);
+  // otherPokemon_type1 = document.getElementById("otherPokemon_info").children[2];
+  // otherPokemon_type2 = document.getElementById("otherPokemon_info").children[3];
+  // otherPokemon_size = parseInt(document.getElementById("otherPokemon_info").children[1].innerHTML);
+
+  // let competingTypes = [yourPokemon_type1, yourPokemon_type2, otherPokemon_type1, otherPokemon_type2];
+  // let competingSizes = [yourPokemon_size,otherPokemon_size];
+  // // console.log(competingTypes)
+  // // console.log(competingTypes[0].innerHTML)
+  // for (i=0; i < competingTypes.length; i++) {
+  //   if (competingTypes[i] != undefined) {
+  //     competingTypes[i] = competingTypes[i].innerHTML;
+  //   }
+  //   else {
+  //     competingTypes[i] = null;
+  //   }
+  // }
+
+  // timeToFight(competingTypes,competingSizes);
+  // // console.log(yourPokemon[3])
+
+}
+
+function prepForCalculations () {
+  yourPokemon_type1 = document.getElementById("yourPokemon_info").children[2];
+  yourPokemon_type2 = document.getElementById("yourPokemon_info").children[3];
+  yourPokemon_size = parseInt(document.getElementById("yourPokemon_info").children[1].innerHTML);
+  otherPokemon_type1 = document.getElementById("otherPokemon_info").children[2];
+  otherPokemon_type2 = document.getElementById("otherPokemon_info").children[3];
+  otherPokemon_size = parseInt(document.getElementById("otherPokemon_info").children[1].innerHTML);
+
+  let competingTypes = [yourPokemon_type1, yourPokemon_type2, otherPokemon_type1, otherPokemon_type2];
+  let competingSizes = [yourPokemon_size,otherPokemon_size];
+  // console.log(competingTypes)
+  // console.log(competingTypes[0].innerHTML)
+  for (i=0; i < competingTypes.length; i++) {
+    if (competingTypes[i] != undefined) {
+      competingTypes[i] = competingTypes[i].innerHTML;
+    }
+    else {
+      competingTypes[i] = null;
+    }
+  }
+
+  timeToFight(competingTypes,competingSizes);
+  // console.log(yourPokemon[3])
+}
+
+function timeToFight (competingTypes,competingSizes) {
+  let yourDamageDelt = (checkTypeAdvantage(competingTypes[0],competingTypes[2])) * (checkTypeAdvantage(competingTypes[0],competingTypes[3])) + (checkTypeAdvantage(competingTypes[1],competingTypes[2])) * (checkTypeAdvantage(competingTypes[1],competingTypes[3]));
+  let otherDamageDelt = (checkTypeAdvantage(competingTypes[2],competingTypes[0])) * (checkTypeAdvantage(competingTypes[2],competingTypes[1])) + (checkTypeAdvantage(competingTypes[3],competingTypes[0])) * (checkTypeAdvantage(competingTypes[3],competingTypes[1]));
+
+  // console.log("yourDamageDelt = " +yourDamageDelt)
+  // console.log("otherDamageDelt = " +otherDamageDelt)
+
+  if (competingSizes[0] > competingSizes[1]) {
+    yourDamageDelt = yourDamageDelt + 0.5;
+  }
+  else if (competingSizes[0] < competingSizes[1]) {
+    otherDamageDelt = otherDamageDelt + 0.5;
+  }
+  else (
+    console.log("tie")
+  )
+
+    infoCount_you = document.getElementById("yourPokemon_info");
+    infoCount_other = document.getElementById("otherPokemon_info");
+    // console.log(infoCount_you)
+
+  let damageYou = `Damage Delt = ${yourDamageDelt}`;
+  let damageOther = `Damage Delt = ${otherDamageDelt}`;
+
+  // console.log(yourDamageDelt);
+  // console.log(otherDamageDelt);
+
+  // console.log("children length you = " + infoCount_you.children.length);
+  // console.log("children length other = " + infoCount_other.children.length)
+
+  if (infoCount_you.children.length == 5) {
+    document.getElementById("yourPokemon_info").children[4].textContent = damageYou;
+  }
+  else {
+    document.getElementById("yourPokemon_info").children[3].textContent = damageYou;
+  }
+  if (infoCount_other.children.length == 5) {
+    document.getElementById("otherPokemon_info").children[4].textContent = damageOther;
+  }
+  else {
+    document.getElementById("otherPokemon_info").children[3].textContent = damageOther;
+  }
+
+
+  // document.getElementById("yourPokemon_info").children[3].textContent = damageYou;
+  // document.getElementById("otherPokemon_info").children[3].textContent = damageOther;
+
+  // console.log(document.getElementById("otherPokemon_info").children[3]);
+  // console.log(damageYou)
+
+  scoreChange(yourDamageDelt,otherDamageDelt);
+}
+
+function scoreChange (yourDamageDelt,otherDamageDelt) {
+  let yourScore = parseInt(document.getElementById("yourPoints").textContent);
+  let otherScore = parseInt(document.getElementById("otherPoints").innerHTML);
+  if (yourDamageDelt > otherDamageDelt) {
+    yourScore += 1;
+    document.getElementById("yourPoints").textContent = yourScore;
+  }
+  else if (yourDamageDelt < otherDamageDelt) {
+    otherScore += 1;
+    document.getElementById("otherPoints").innerHTML = otherScore;
+  }
+  else {
+    console.log("tie");
+  }
+
+  // console.log(yourScore,otherScore)
+};
+
+function clearPokemon () {
+  let otherPokemon = document.getElementById("otherPokemon_img").children;
+  // console.log(otherPokemon)
+  if (otherPokemon.length != 0) {
+    document.getElementById("otherPokemon_img").innerHTML = "";
+    document.getElementById("otherPokemon_info").innerHTML = "";
+  }
+}
+
+
 mainButtons();
